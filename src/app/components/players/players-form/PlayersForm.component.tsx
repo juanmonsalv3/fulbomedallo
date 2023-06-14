d0e06d7cfc25408f9d00ba9c69e06d1e1fdcd5c1
Juan Monsalve <j.monsalve@astoundcommerce.com>
2023-06-14 11:55:54 -0500

    organize players modals

--- src/app/components/players/players-form/PlayersForm.component.tsx
+++ src/app/components/players/players-form/PlayersForm.component.tsx
@@ -1,10 +1,10 @@
-import {useFormik} from 'formik';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import {Button, Grid, TextField} from '@mui/material';
+import { Button, Grid, Stack, TextField } from '@mui/material';
 import axios from 'axios';
-import {toast} from "react-hot-toast";
-import {FormEvent, useEffect} from "react";
-import {Player} from "@/types/players";
+import { toast } from 'react-hot-toast';
+import { FormEvent, useEffect } from 'react';
+import { Player } from '@/types/players';
 
 const initialValues = {
   name: '',
@@ -20,7 +20,9 @@ const validationSchema = Yup.object({
   name: Yup.string().required('El nombre es requerido'),
   lastName: Yup.string().required('Los apellidos son requeridos'),
   telephone: Yup.string().required('El teléfono es requerido'),
-  email: Yup.string().email('Correo electrónico inválido').required('El correo electrónico es requerido'),
+  email: Yup.string()
+    .email('Correo electrónico inválido')
+    .required('El correo electrónico es requerido'),
   jerseyNumber: Yup.number().required('El número dorsal es requerido'),
   position: Yup.string().required('La posición es requerida'),
   nickname: Yup.string().required('El nickname es requerido'),
@@ -50,11 +52,20 @@ const formikState = {
   },
 };
 
-interface PlayerFormProps { player?: Player, onUpdatePlayerList: () => void, closeEdit?: () => void }
+interface PlayerFormProps {
+  player?: Player;
+  onUpdatePlayerList: () => void;
+  closeEdit?: () => void;
+}
 
-export const PlayersForm = ({ player, onUpdatePlayerList, closeEdit }: PlayerFormProps) => {
+export const PlayersForm = ({
+  player,
+  onUpdatePlayerList,
+  closeEdit,
+}: PlayerFormProps) => {
   const formik = useFormik(formikState);
-  const submit =  async (e: FormEvent) => {
+  const { setValues } = formik;
+  const submit = async (e: FormEvent) => {
     try {
       e.preventDefault();
       formik.resetForm();
@@ -78,18 +89,18 @@ export const PlayersForm = ({ player, onUpdatePlayerList, closeEdit }: PlayerFor
 
   useEffect(() => {
     if (player) {
-      formik.setValues({ ...player });
+      setValues({ ...player });
     }
-  }, [formik, player]);
+  }, [player, setValues]);
 
   return (
-    <form onSubmit={submit} style={{paddingTop: '20px'}}>
+    <form onSubmit={submit} style={{ paddingTop: '20px' }}>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="name"
-            name="name"
-            label="Nombre"
+            id='name'
+            name='name'
+            label='Nombre'
             fullWidth
             value={formik.values.name}
             onChange={formik.handleChange}
@@ -99,9 +110,9 @@ export const PlayersForm = ({ player, onUpdatePlayerList, closeEdit }: PlayerFor
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="lastName"
-            name="lastName"
-            label="Apellidos"
+            id='lastName'
+            name='lastName'
+            label='Apellidos'
             fullWidth
             value={formik.values.lastName}
             onChange={formik.handleChange}
@@ -111,9 +122,9 @@ export const PlayersForm = ({ player, onUpdatePlayerList, closeEdit }: PlayerFor
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="telephone"
-            name="telephone"
-            label="Teléfono"
+            id='telephone'
+            name='telephone'
+            label='Teléfono'
             fullWidth
             value={formik.values.telephone}
             onChange={formik.handleChange}
@@ -123,9 +134,9 @@ export const PlayersForm = ({ player, onUpdatePlayerList, closeEdit }: PlayerFor
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="email"
-            name="email"
-            label="Correo electrónico"
+            id='email'
+            name='email'
+            label='Correo electrónico'
             fullWidth
             value={formik.values.email}
             onChange={formik.handleChange}
@@ -135,22 +146,26 @@ export const PlayersForm = ({ player, onUpdatePlayerList, closeEdit }: PlayerFor
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="jerseyNumber"
-            name="jerseyNumber"
-            label="Número dorsal"
-            type="number"
+            id='jerseyNumber'
+            name='jerseyNumber'
+            label='Número dorsal'
+            type='number'
             fullWidth
             value={formik.values.jerseyNumber}
             onChange={formik.handleChange}
-            error={formik.touched.jerseyNumber && Boolean(formik.errors.jerseyNumber)}
-            helperText={formik.touched.jerseyNumber && formik.errors.jerseyNumber}
+            error={
+              formik.touched.jerseyNumber && Boolean(formik.errors.jerseyNumber)
+            }
+            helperText={
+              formik.touched.jerseyNumber && formik.errors.jerseyNumber
+            }
           />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            id="position"
-            name="position"
-            label="Posición"
+            id='position'
+            name='position'
+            label='Posición'
             fullWidth
             value={formik.values.position}
             onChange={formik.handleChange}
@@ -160,9 +175,9 @@ export const PlayersForm = ({ player, onUpdatePlayerList, closeEdit }: PlayerFor
         </Grid>
         <Grid item xs={12}>
           <TextField
-            id="nickname"
-            name="nickname"
-            label="Alias"
+            id='nickname'
+            name='nickname'
+            label='Alias'
             fullWidth
             value={formik.values.nickname}
             onChange={formik.handleChange}
@@ -171,11 +186,18 @@ export const PlayersForm = ({ player, onUpdatePlayerList, closeEdit }: PlayerFor
           />
         </Grid>
         <Grid item xs={12}>
-          <Button type="submit" variant="contained" color="primary" disabled={!formik.isValid}>
-            Enviar
-          </Button>
+          <Stack
+            direction='row'
+            justifyContent='flex-end'
+            alignItems='center'
+            spacing={2}
+          >
+            <Button type='submit' color='primary' disabled={!formik.isValid}>
+              Guardar
+            </Button>
+          </Stack>
         </Grid>
       </Grid>
     </form>
   );
-}
+};
