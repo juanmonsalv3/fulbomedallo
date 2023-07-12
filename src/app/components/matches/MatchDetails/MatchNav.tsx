976811c777e518e5b35c58002ad4cf66f5b4d560
Juan Monsalve <juanchomonsalve92@gmail.com>
2023-07-12 10:38:52 -0500

    tabs

--- src/app/components/matches/MatchDetails/MatchNav.tsx
+++ src/app/components/matches/MatchDetails/MatchNav.tsx
@@ -1,15 +1,7 @@
 import * as React from 'react'
 import Box from '@mui/material/Box'
-import Stepper from '@mui/material/Stepper'
-import Step from '@mui/material/Step'
-import StepLabel from '@mui/material/StepLabel'
-import StepContent from '@mui/material/StepContent'
-import Button from '@mui/material/Button'
-import Paper from '@mui/material/Paper'
-import Typography from '@mui/material/Typography'
-import { StepButton, Tab, Tabs } from '@mui/material'
+import { Tab, Tabs } from '@mui/material'
 import { Match } from '@/types/matches'
-import MatchPlayersList from './MatchPlayersList'
 import MatchTeams from './MatchTeams'
 import MatchEvents from './MatchEvents'
 
@@ -18,54 +10,10 @@ interface Props {
 }
 
 export default function MatchNav({ matchData }: Props) {
-  const [activeStep, setActiveStep] = React.useState(0)
-
-  const handleNext = (index: number) => {
-    return () => setActiveStep(index)
-  }
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1)
-  }
-
-  const handleReset = () => {
-    setActiveStep(0)
-  }
-
-  // return (
-  //   <Box>
-  //     <Stepper activeStep={activeStep} sx={{ mb: 4 }}>
-  //       <Step>
-  //         <StepButton color="inherit" onClick={handleNext(0)}>
-  //           <StepLabel>Lista de Jugadores</StepLabel>
-  //         </StepButton>
-  //       </Step>
-  //       <Step>
-  //         <StepButton color="inherit" onClick={handleNext(1)}>
-  //           <StepLabel>Equipos</StepLabel>
-  //         </StepButton>
-  //       </Step>
-  //       <Step>
-  //         <StepButton color="inherit" onClick={handleNext(2)}>
-  //           <StepLabel>Eventos</StepLabel>
-  //         </StepButton>
-  //       </Step>
-  //     </Stepper>
-  //     {activeStep === 0 && (
-  //       <MatchPlayersList
-  //         matchData={matchData}
-  //         onSave={() => setActiveStep(1)}
-  //       />
-  //     )}
-  //     {activeStep === 1 && <MatchTeams />}
-  //     {activeStep === 2 && <MatchEvents />}
-  //   </Box>
-  // )
-
-  const [value, setValue] = React.useState(0)
+  const [activeTab, setActiveTab] = React.useState(0)
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue)
+    setActiveTab(newValue)
   }
 
   function TabPanel({
@@ -77,7 +25,7 @@ export default function MatchNav({ matchData }: Props) {
         sx={{
           my: 2,
         }}
-        display={index === value ? 'block' : 'none'}
+        display={index === activeTab ? 'block' : 'none'}
       >
         {children}
       </Box>
@@ -87,7 +35,7 @@ export default function MatchNav({ matchData }: Props) {
   return (
     <Box>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange}>
+        <Tabs value={activeTab} onChange={handleChange}>
           <Tab label="Equipos" />
           <Tab label="Eventos" />
         </Tabs>
